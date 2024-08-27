ee1080ff300f59c29dd81b953beffdde599d775f
ericksilva232323 <erick14silva14@gmail.com>
2024-08-27 08:30:25 -0300

    up

--- singIn.js
+++ singIn.js
@@ -1,2 +1,11 @@
+function openModal () {
+    const modal = document.getElementById('windowModal')
 
+    modal.classList.add('abrir')
 
+    modal.addEventListener( 'click', (e) => {
+        if( e.target.id == 'close' || e.target.id == 'windowModal' ){
+            modal.classList.remove('abrir')
+        }
+    })
+}
