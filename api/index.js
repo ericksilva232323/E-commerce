28a50ad4d778e13a06b3cf6b589560d1727ce569
jaquejak <jaqueline.44rodrigues@gmail.com>
2024-09-16 21:33:27 -0300

    ptm

--- api/index.js
+++ api/index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`respondendo`);
+  console.log(`respondendo na porta`);
 });
--- register/singUp.js
+++ register/singUp.js
@@ -167,3 +167,5 @@ app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
 });
 
+
+
