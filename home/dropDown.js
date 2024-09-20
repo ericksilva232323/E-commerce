c1421070058bfb3b6acfff07e13db4f4ceea7901
ericksilva232323 <erick14silva14@gmail.com>
2024-09-20 08:02:10 -0300

    up

--- home/dropDown.js
+++ home/dropDown.js
@@ -1,7 +1,7 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function toggleDropdown() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function toggleDropdown(myDropdownId) {
+    document.getElementById(myDropdownId).classList.toggle("show");
   }
   
   // Close the dropdown menu if the user clicks outside of it
