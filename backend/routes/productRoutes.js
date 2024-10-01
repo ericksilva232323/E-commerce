6ba3b043d62bddf63a9c20be69aa57b492109ed6
ericksilva232323 <erick14silva14@gmail.com>
2024-10-01 08:58:43 -0300

    up

--- backend/routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { listProducts, addProduct } = require('../controllers/productController');
-const router = express.Router();
-
-router.get('/', listProducts);
-router.post('/', addProduct);
-
-module.exports = router;
