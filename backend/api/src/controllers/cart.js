63ae9d2f4c3434859f000a9e5e6966d7c70baf16
ericksilva232323 <erick14silva14@gmail.com>
2024-12-02 09:39:39 -0300

    up

---
+++ backend/api/src/controllers/cart.js
@@ -0,0 +1,24 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+require('dotenv').config();
+
+const read = async (req, res) => {
+    if(req.params.id !== undefined){
+        const cart = await prisma.cart.findMany({
+            where: {
+                cart: parseInt(req.params.id)
+            },
+            orderBy:{
+                data:'desc'
+            }
+        });
+        return res.json(cart);
+    }else{
+        const cart = await prisma.cart.findMany();
+        return res.json(cart);
+    }
+}
+
+module.export = {
+    read
+}
---
+++ backend/api/src/controllers/like.js
@@ -0,0 +1,25 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+require('dotenv').config();
+
+const read = async (req, res) => {
+    if(req.params.id !== undefined){
+        const like = await prisma.like.findMany({
+            where: {
+                like: parseInt(req.paramns.id)
+            },
+            orderBy:{
+                data:'desc'
+            }
+        });
+        return res.json(like);
+    }else{
+        const product = await prisma.like.findMany();
+        return res.json(product);
+    }
+}
+
+module.exports = {
+    read
+}
+
