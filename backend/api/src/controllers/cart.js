04c06c6817b03eeb235bef59d874fbb6a408b5d7
ericksilva232323 <erick14silva14@gmail.com>
2024-12-03 09:54:19 -0300

    up

--- backend/api/src/controllers/cart.js
+++ backend/api/src/controllers/cart.js
@@ -1,8 +1,14 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+// const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const read = async (req, res) => {
+
+    if (!req.user) {
+        return res.status(401).json({ message: 'Usuário não autenticado' });
+    }
+
     if(req.params.id !== undefined){
         const cart = await prisma.cart.findMany({
             where: {
@@ -19,6 +25,23 @@ const read = async (req, res) => {
     }
 }
 
+// const auth = (req, res, next) => {
+//     const token = req.header.authorization?.split(' ')[1];
+
+//     if(!token){
+//         return res.status(401).json({message:'Token not provide'});
+//     }
+
+//     try{
+//         const user = verifyToken(token);
+//         req.user = user;
+//         next();
+//     } catch(error){
+//         return res.status(401).json({message:'Token invalid'});
+//     }
+// };
+
 module.export = {
-    read
+    read,
+    // auth
 }
--- backend/api/src/routes.js
+++ backend/api/src/routes.js
@@ -1,17 +1,23 @@
 const express = require('express');
-
 const router = express.Router();
+// const { Auth } = require('./controllers/middleware/middleware');
 
-// const Cart = require('./controllers/cart');
-// const Like = require('./controllers/like');
-// const Pay = require('./controllers/pay');
+const Cart = require('./controllers/cart');
+const Like = require('./controllers/like');
 const Product = require('./controllers/product');
 const User = require('./controllers/user');
+
 
 router.get('/user', User.read);
 router.get('/user/:id', User.read);
 
 router.get('/product', Product.read);
+
+router.get('/like', Like.read);
+// router.get('/like', Auth, read);
+
+router.get('/cart', Cart.read);
+// router.get('/cart', Auth, read);
 
 router.get('/',(req, res) => { return res.json("API FLOW(E-COMMERCE) respondendo")});
 
